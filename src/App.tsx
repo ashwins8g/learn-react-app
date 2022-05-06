e057625414811c92ab7d92d9a2da35be9ecc2595
george_ashwin <george.ashwin@outlook.com>
2022-05-06 09:05:02 +0530

    Initial Commit- Airbnb style guides integration, husky, tailwind, and Sass configurations.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <h1 className="text-lg font-medium text-sky-500 dark:text-sky-400">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
